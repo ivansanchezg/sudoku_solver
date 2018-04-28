d527654153bb5269b89514adaf98145744245747
Ivan Sanchez Gutmann <ivanchezg@gmail.com>
2018-04-28 09:32:33 -0500

    Add new tests for matrix validation

--- src/test/java/com/ivansanchezg/sudokusolver/SudokuSolverTest.java
+++ src/test/java/com/ivansanchezg/sudokusolver/SudokuSolverTest.java
@@ -8,28 +8,18 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
+
 public class SudokuSolverTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
+
     public SudokuSolverTest(String testName) {
         super(testName);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite() {
         return new TestSuite( SudokuSolverTest.class );
     }
 
     public void testBlankMatrix() {
-
         int[][] matrix = {
 			{0,0,0,0,0,0,0,0,0},
 			{0,0,0,0,0,0,0,0,0},
@@ -41,12 +31,10 @@ public class SudokuSolverTest extends TestCase {
 			{0,0,0,0,0,0,0,0,0},
 			{0,0,0,0,0,0,0,0,0}
         };
-        
         assertTrue(SudokuSolver.solve(matrix));
     }
 
     public void testMatrixWithSolution1() {
-
         int[][] matrix = {
 			{0,0,0,2,6,0,7,0,1},
 			{6,8,0,0,7,0,0,9,0},
@@ -58,12 +46,10 @@ public class SudokuSolverTest extends TestCase {
 			{0,4,0,0,5,0,0,3,6},
 			{7,0,3,0,1,8,0,0,0}
 		};
-        
         assertTrue(SudokuSolver.solve(matrix));
     }
 
     public void testMatrixWithSolution2() {
-
         int[][] matrix = {
 			{1,0,0,4,8,9,0,0,6},
 			{7,3,0,0,0,0,0,4,0},
@@ -75,12 +61,10 @@ public class SudokuSolverTest extends TestCase {
 			{0,2,0,0,0,0,0,3,7},
 			{8,0,0,5,1,2,0,0,4}
 		};
-        
         assertTrue(SudokuSolver.solve(matrix));
     }
 
     public void testMatrixWithSolution3() {
-
         int[][] matrix = {
 			{0,0,0,6,0,0,4,0,0},
 			{7,0,0,0,0,3,6,0,0},
@@ -92,12 +76,10 @@ public class SudokuSolverTest extends TestCase {
 			{9,0,3,0,0,0,0,0,0},
 			{0,2,0,0,0,0,1,0,0}
 		};
-        
         assertTrue(SudokuSolver.solve(matrix));
     }
 	
 	public void testMatrixWithNoSolution() {
-		
 		int[][] matrix = {
 			{0,0,6,6,0,0,4,0,0},
 			{7,0,0,0,0,3,6,0,0},
@@ -109,15 +91,12 @@ public class SudokuSolverTest extends TestCase {
 			{9,0,3,0,0,0,0,0,0},
 			{0,2,0,0,0,0,1,0,0}
 		};
-		
 		assertFalse(SudokuSolver.solve(matrix));
     }
     
     public void testMatrixWithSolutionUsingJson() {
-
         String filePath = new File("").getAbsolutePath();
         filePath = filePath.concat("/src/test/java/com/ivansanchezg/sudokusolver/sudoku.json");
         assertTrue(SudokuSolver.solveWithJson(filePath));
-
     }
 }
--- src/test/java/com/ivansanchezg/sudokusolver/SudokuValidatorTest.java
+++ src/test/java/com/ivansanchezg/sudokusolver/SudokuValidatorTest.java
@@ -10,94 +10,72 @@ public class SudokuValidatorTest extends TestCase {
         super(testName);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
+
     public static Test suite() {
         return new TestSuite(SudokuValidatorTest.class);
     }
 
     public void testCheckColumnWithValidColumn() {
-
         int[][] matrix = {
 			{1,0,0,0},
 			{0,0,0,0},
 			{2,0,0,0},
 			{0,0,0,0}
         };
-
-
         assertTrue(SudokuValidator.checkColumn(matrix, 1, matrix[0].length, 0, 0));
     }
 
     public void testCheckColumnWithInvalidColumn() {
-
         int[][] matrix = {
 			{1,0,0,0},
 			{0,0,0,0},
 			{1,0,0,0},
 			{0,0,0,0}
         };
-
         assertFalse(SudokuValidator.checkColumn(matrix, 1, matrix[0].length, 0, 0));
     }
 
     public void testCheckRowWithValidRow() {
-
         int[][] matrix = {
 			{1,2,3,0},
 			{0,0,0,0},
 			{0,0,0,0},
 			{0,0,0,0}
         };
-
-
         assertTrue(SudokuValidator.checkRow(matrix, 1, matrix[0].length, 0, 0));
     }
 
     public void testCheckRowWithInvalidRow() {
-
         int[][] matrix = {
 			{1,2,1,0},
 			{0,0,0,0},
 			{0,0,0,0},
 			{0,0,0,0}
         };
-
-
         assertFalse(SudokuValidator.checkRow(matrix, 1, matrix[0].length, 0, 0));
     }
 
     public void testCheckSquareWithValidSquare() {
-
         int[][] matrix = {
 			{2,0,0,0},
 			{0,1,0,0},
 			{0,0,0,0},
 			{0,0,0,0}
         };
-
-
         assertTrue(SudokuValidator.checkSquare(matrix, 1, matrix[0].length, 1, 1));
-
     }
 
     public void testCheckSquareWithInvalidSquare() {
-
         int[][] matrix = {
 			{1,0,0,0},
 			{0,1,0,0},
 			{0,0,0,0},
 			{0,0,0,0}
         };
-
-
         assertFalse(SudokuValidator.checkSquare(matrix, 1, matrix[0].length, 1, 1));
-
     }
 
     public void testCheckSquareWithValidSquare9x9() {
-        
         int[][] matrix = {
 			{0,0,0,5,0,3,0,0,0},
 			{0,0,0,0,2,0,0,0,0},
@@ -109,14 +87,10 @@ public class SudokuValidatorTest extends TestCase {
 			{0,0,0,0,0,0,0,0,0},
 			{0,0,0,0,0,0,0,0,0}
         };
-
-
         assertTrue(SudokuValidator.checkSquare(matrix, 1, matrix[0].length, 2, 3));
-
     }
 
     public void testCheckSquareWithInvalidSquare9x9() {
-
         int[][] matrix = {
 			{0,0,0,5,0,3,0,0,0},
 			{0,0,0,0,2,1,0,0,0},
@@ -128,8 +102,80 @@ public class SudokuValidatorTest extends TestCase {
 			{0,0,0,0,0,0,0,0,0},
 			{0,0,0,0,0,0,0,0,0}
         };
-
         assertFalse(SudokuValidator.checkSquare(matrix, 1, matrix[0].length, 2, 3));
+    }
 
+    public void testIsValidMatrixWithValidMatrix() {
+        int[][] matrix = {
+			{0,0,0,2,6,0,7,0,1},
+			{6,8,0,0,7,0,0,9,0},
+			{1,9,0,0,0,4,5,0,0},
+			{8,2,0,1,0,0,0,4,0},
+			{0,0,4,6,0,2,9,0,0},
+			{0,5,0,0,0,3,0,2,8},
+			{0,0,9,3,2,0,0,7,4},
+			{0,4,0,0,5,0,0,3,6},
+			{7,0,3,0,1,8,0,0,0}
+        };
+        assertTrue(SudokuValidator.isValidMatrix(matrix));
+    }
+
+    public void testIsValidMatrixWithNullMatrix() {
+        int[][] matrix = null;
+        assertFalse(SudokuValidator.isValidMatrix(matrix));
+    }
+
+    public void testIsValidMatrixWithNxMMatrix1() {
+        int[][] matrix = {
+			{0,0,0,2,6,0,7,0,1},
+			{6,8,0,0,7,0,0,9,0},
+			{1,9,0,0,0,4,5,0,0},
+        };
+        assertFalse(SudokuValidator.isValidMatrix(matrix));
+    }
+
+    public void testIsValidMatrixWithNxMMatrix2() {
+        int[][] matrix = {
+			{0,0,0,2,6,0},
+			{6,8,0,0,7,0},
+			{1,9,0,0,0,4},
+			{8,2,0,1,0,0},
+			{0,0,4,6,0,2},
+			{0,5,0,0,0,3},
+			{0,0,9,3,2,0},
+			{0,4,0,0,5,0},
+			{7,0,3,0,1,8}
+        };
+        assertFalse(SudokuValidator.isValidMatrix(matrix));
+    }
+
+    public void testIsValidMatrixWithNumberAboveRange() {
+        int[][] matrix = {
+			{0,0,0,2,6,0,7,0,1},
+			{6,8,0,0,7,0,0,9,0},
+			{1,9,0,0,0,4,5,0,0},
+			{8,2,0,1,11,0,0,4,0},
+			{0,0,4,6,0,2,9,0,0},
+			{0,5,0,0,0,3,0,2,8},
+			{0,0,9,3,2,0,0,7,4},
+			{0,4,0,0,5,0,0,3,6},
+			{7,0,3,0,1,8,0,0,0}
+        };
+        assertFalse(SudokuValidator.isValidMatrix(matrix));
+    }
+
+    public void testIsValidMatrixWithNumberBelowRange() {
+        int[][] matrix = {
+			{0,0,0,2,6,0,7,0,1},
+			{6,8,0,0,7,0,0,9,0},
+			{1,9,0,0,0,4,5,0,0},
+			{8,2,0,1,-1,0,0,4,0},
+			{0,0,4,6,0,2,9,0,0},
+			{0,5,0,0,0,3,0,2,8},
+			{0,0,9,3,2,0,0,7,4},
+			{0,4,0,0,5,0,0,3,6},
+			{7,0,3,0,1,8,0,0,0}
+        };
+        assertFalse(SudokuValidator.isValidMatrix(matrix));
     }
 }
